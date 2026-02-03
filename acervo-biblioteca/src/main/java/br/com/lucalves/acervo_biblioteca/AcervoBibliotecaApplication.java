ad3072affb869d35ef03a291a00483f6bb6f747e
Lucas Alves <olalvezluc@gmail.com>
2026-02-02 21:10:32 -0300

    Commit inicial

---
+++ acervo-biblioteca/src/main/java/br/com/lucalves/acervo_biblioteca/AcervoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucalves.acervo_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcervoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcervoBibliotecaApplication.class, args);
+	}
+
+}
---
+++ acervo-biblioteca/src/test/java/br/com/lucalves/acervo_biblioteca/AcervoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucalves.acervo_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
